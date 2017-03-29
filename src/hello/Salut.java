f9685d798ab212814317159cf25da34b6e8c45d2
Unknown <loua25n_raphael@yahoo.fr>
2017-03-29 21:25:44 +0200

    salut programm

---
+++ src/hello/Salut.java
@@ -0,0 +1,25 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Salut {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int x = 0;
+		
+		System.out.println(x);
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int y = scanner.nextInt();
+		
+		System.out.println(y);
+
+	}
+
+}
